ee540c0ce658f1b7d079b8312ce517065e7ff731
heejung <cheej10@naver.com>
2022-03-27 13:35:37 +0900

    img 경로 수정

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -31,7 +31,7 @@ export default function Nav() {
     <nav className={`nav ${show ? 'nav_black' : ''}`}>
       <img
         className="netflix_logo"
-        src="/img/netflix_logo.png"
+        src="https://cheej10.github.io/netflix-clone/public/img/netflix_logo.png"
         alt="netflix logo"
         onClick={() => window.location.reload()}
         width="180"
@@ -45,7 +45,7 @@ export default function Nav() {
       />
       <img
         className="netflix_avatar"
-        src="/img/netflix_avatar.png"
+        src="https://cheej10.github.io/netflix-clone/public/img/netflix_avatar.png"
         alt="netflix avatar"
       />
     </nav>
